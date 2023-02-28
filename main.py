f219d2b013ae67ef48380d36b37704e7d7e40094
Jonghwan Na <95013904+JohnSodium@users.noreply.github.com>
2023-02-28 16:51:55 +0900

    Update main.py
    As time goes on, some vehicles exit the frame. Re-add those vehicles for consistency with 0 weight so it has no impact.

--- main.py
+++ main.py
@@ -2,15 +2,20 @@ import os
 import pandas as pd
 import count_lc
 import GCN
+import CNN
 
 
 prev_frame = None
 node_data = None
+vehicle_count = 0
+spatial_features = []
+original_vehicles = []
+added_index = []
 
 ###### load data ######
 lc_frame = count_lc.get_Swap_Frame()    # Frames where lane changes occur
 
-for i in range(1):
+for i in range(0, 1):
 
     source_features = []
     target_features = []
@@ -20,10 +25,10 @@ for i in range(1):
     if not os.path.exists(new_path):
         os.makedirs(new_path)
 
-    feature_vector = pd.read_csv(f"./new_input_feature_matrix_20maxy/"
+    feature_vector = pd.read_csv(f"./new_input_feature_matrix_5m10m/"
                                  f"{lc_frame[i]}_features_with_label.csv")
 
-    for j in range(len(feature_vector)):
+    for j in range(len(feature_vector) - 1, -1, -1):
         cur_frame = int(feature_vector["Frame"][j])
 
         if cur_frame != prev_frame:
@@ -36,7 +41,7 @@ for i in range(1):
             edges = []
             adjacency = []
 
-            node_data = pd.read_csv(f"./adjacency_matrix(5-10)/{cur_frame}_output.csv")
+            node_data = pd.read_csv(f"./adjacency_5m10m/{cur_frame}_output.csv")
 
             # Find connections between vehicles using the adjacency matrix
             for index, row in node_data.iterrows():
@@ -53,6 +58,14 @@ for i in range(1):
                     connection.append((row[k]))
                 adjacency.append(connection)
 
+            if vehicle_count == 0:
+                vehicle_count = len(vehicle_list)
+
+            vehicle_list = sorted(vehicle_list)
+
+            if j == len(feature_vector) - 1:
+                original_vehicles = vehicle_list
+
             for k in range(len(vehicle_list)):
                 separate_by_id = feature_vector[feature_vector['Vehicle_ID'] == vehicle_list[k]]
                 separate_by_frame = separate_by_id[separate_by_id['Frame'] == cur_frame]
@@ -66,9 +79,33 @@ for i in range(1):
             for k in temp_y:
                 y.append(float(k))
 
+            originalptr = 0
+            curptr = 0
+            for k in range(len(original_vehicles)):
+                # print(originalptr, curptr, vehicle_list, original_vehicles)
+                if original_vehicles[originalptr] == vehicle_list[curptr]:
+                    originalptr += 1
+                    curptr += 1
+                elif original_vehicles[originalptr] != vehicle_list[curptr]:
+                    # print('missing vehicle')
+                    vehicle_list.insert(curptr, original_vehicles[originalptr])
+                    x.insert(curptr, 0)
+                    y.insert(curptr, 0)
+                    adjacency.insert(curptr, [0 for i in range(len(vehicle_list))])
+                    added_index.append(curptr)
+                    for l in range(len(vehicle_list)):
+                        if len(adjacency[l]) != len(vehicle_list):
+                            adjacency[l].insert(curptr, 0)
+
+                    originalptr += 1
+                    curptr += 1
+
             # Self Loop
             for k in range(len(adjacency)):
-                adjacency[k][k] = 1
+                if k not in added_index:
+                    adjacency[k][k] = 1
 
-            result = GCN.graph_convolution(cur_frame, vehicle_list, x, y, adjacency)
+            result = GCN.setup(cur_frame, vehicle_list, x, y, adjacency)
+            spatial_features.append(result)
+
             prev_frame = cur_frame
